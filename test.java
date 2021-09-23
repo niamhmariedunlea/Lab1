df9b158ae1729bced14b1ff56eaf1f42442973a6
anonymous <anonymous@ul.ie>
2021-09-23 14:23:53 +0100

    test file add

---
+++ test.java
@@ -0,0 +1,19 @@
+package CS4004;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Niamh
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextString();
+
+
+}
